9adc1b6f344b23cd3a2df5d16f7e70d492e83120
Skelzo <56433452+Skelzo@users.noreply.github.com>
2020-06-14 12:35:33 +0200

    Add files via upload

---
+++ INF Aufgabe/INF Aufgabe/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace INF_Aufgabe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.lblMiliseconds = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtRandomNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(64, 125);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(183, 125);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(299, 125);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutes.Location = new System.Drawing.Point(143, 65);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(44, 31);
+            this.lblMinutes.TabIndex = 3;
+            this.lblMinutes.Text = "00";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.Location = new System.Drawing.Point(214, 65);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(44, 31);
+            this.lblSeconds.TabIndex = 4;
+            this.lblSeconds.Text = "00";
+            // 
+            // lblMiliseconds
+            // 
+            this.lblMiliseconds.AutoSize = true;
+            this.lblMiliseconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiliseconds.Location = new System.Drawing.Point(291, 65);
+            this.lblMiliseconds.Name = "lblMiliseconds";
+            this.lblMiliseconds.Size = new System.Drawing.Size(44, 31);
+            this.lblMiliseconds.TabIndex = 5;
+            this.lblMiliseconds.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(193, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(264, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = ".";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtRandomNumber
+            // 
+            this.txtRandomNumber.Location = new System.Drawing.Point(172, 176);
+            this.txtRandomNumber.Name = "txtRandomNumber";
+            this.txtRandomNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtRandomNumber.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 242);
+            this.Controls.Add(this.txtRandomNumber);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblMiliseconds);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label lblMiliseconds;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtRandomNumber;
+    }
+}
+
---
+++ INF Aufgabe/INF Aufgabe/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INF_Aufgabe
+{
+    public partial class Form1 : Form
+    {
+        int timeMilliseconds;
+        int timeSeconds;
+        int timeMinutes;
+        int randomNumber;
+        bool IsActive;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetTime();
+            IsActive = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            IsActive = true;
+            RandomNumber();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            IsActive = false;
+            RandomNumber();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            IsActive = false;
+
+            ResetTime();
+            RandomNumber();
+        }
+
+        private void ResetTime()
+        {
+            timeMilliseconds = 0;
+            timeSeconds = 0;
+            timeMinutes = 0;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsActive)
+            {
+                timeMilliseconds++;
+
+                if (timeMilliseconds > 100)
+                {
+                    timeSeconds++;
+                    timeMilliseconds = 0;
+
+                    if (timeSeconds > 60)
+                    {
+                        timeMinutes++;
+                        timeSeconds = 0;
+                    }
+                }
+            }
+
+            DrawTime();
+        }
+
+        private void DrawTime()
+        {
+            lblMiliseconds.Text = String.Format("{0:00}", timeMilliseconds);
+            lblSeconds.Text = String.Format("{0:00}", timeSeconds);
+            lblMinutes.Text = String.Format("{0:00}", timeMinutes);
+        }
+
+        public void RandomNumber()
+        {
+            Random rndm = new Random();
+            randomNumber = rndm.Next(1, 1000);
+
+            txtRandomNumber.Text = Convert.ToString(randomNumber);
+        }
+    }
+}
